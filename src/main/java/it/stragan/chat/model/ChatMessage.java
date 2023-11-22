197a22f8b1c6aa2c0d34557a3384e59b0abcff9e
pszczepkowski <p.szczepkowski87@gmail.com>
2023-11-22 14:09:45 +0100

    init 3

--- src/main/java/it/stragan/chat/model/ChatMessage.java
+++ src/main/java/it/stragan/chat/model/ChatMessage.java
@@ -7,11 +7,13 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @DynamoDBTable(tableName = "ChatMessage")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Setter
 public class ChatMessage {
 
     private String id;
